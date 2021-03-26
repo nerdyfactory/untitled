021764969b66108b5ab8293c0ea78dd4b52d6bc8
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2021-03-26 14:31:42 +0500

    feat: show photos in bottom list from map on marker click

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -6,6 +6,7 @@ import 'package:flutter/rendering.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:untitled/utils/PhotoQuery.dart';
 import 'package:untitled/widgets.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -34,7 +35,7 @@ class _MapState extends State<Map> {
             cameraPositionForFetchingRecords.longitude + 5));
     _photos = await query.getPhotos();
 
-    _photos.forEach((user) {
+    _photos.forEach((photo) {
       _markerIconKeys.add(GlobalKey());
     });
     setState(() {});
@@ -105,20 +106,64 @@ class _MapState extends State<Map> {
     );
   }
 
-  void _addMarker(PhotoData user, int index) async {
+  void _addMarker(PhotoData photo, int index) async {
     Future.delayed(Duration(seconds: 1)).then((value) async {
-      if (renderedMarkers.contains(user.url)) return;
+      if (renderedMarkers.contains(photo.url)) return;
 
       var markerBytes = await _getMarkerIconBytes(_markerIconKeys[index]);
       if (markerBytes == null) return;
 
       _markers.add(Marker(
-          markerId: MarkerId(user.url),
-          position: user.location,
+          markerId: MarkerId(photo.url),
+          position: photo.location,
+          onTap: () {
+            this._printMasog(photo);
+          },
           icon: BitmapDescriptor.fromBytes(markerBytes)));
-      renderedMarkers.add(user.url);
+      renderedMarkers.add(photo.url);
       setState(() {});
     });
+  }
+
+  _printMasog(PhotoData photo) {
+    List<PhotoData> photosAtSameLocation = [..._photos];
+    photosAtSameLocation = photosAtSameLocation
+        .where((e) => (e.location.latitude.toStringAsPrecision(4).compareTo(
+                    photo.location.latitude
+                        .toStringAsPrecision(4)
+                        .toString()) ==
+                0 &&
+            e.location.longitude.toStringAsPrecision(4).compareTo(photo
+                    .location.longitude
+                    .toStringAsPrecision(4)
+                    .toString()) ==
+                0 &&
+            e.url != photo.url))
+        .toList();
+    List<PhotoContainer> photoContainers = photosAtSameLocation
+        .map((e) => PhotoContainer(path: e.url, marginTop: 5, height: 262))
+        .toList();
+
+    showMaterialModalBottomSheet(
+      context: context,
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+      ),
+      builder: (context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+            height: MediaQuery.of(context).size.height * 0.85,
+            child: ListView(
+              children: [
+                PhotoContainer(path: photo.url, marginTop: 5, height: 262),
+                ...photoContainers
+              ],
+            )),
+      ),
+    );
   }
 
   Future<Uint8List?> _getMarkerIconBytes(GlobalKey markerKey) async {
