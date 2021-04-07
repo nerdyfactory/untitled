c6282738f5c9b90030a4a9fbc27a68af8783190c
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2021-04-07 16:25:35 +0500

    feat: sorted feedscreen photos based on distance

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,9 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:untitled/routes.dart';
 import 'package:untitled/widgets.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
+  @override
+  _FeedState createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  late LatLng userLocation = LatLng(0.0, 0.0);
+
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -41,20 +55,35 @@ class Feed extends StatelessWidget {
                 AddLocationIcon()
               ],
             );
+          List photoList = snapshot.data!.docs.map((document) {
+            var distance = Geolocator.distanceBetween(
+                document.data()!['location'].latitude,
+                document.data()!['location'].longitude,
+                userLocation.latitude,
+                userLocation.longitude);
+            var photo = {
+              'path': document.data()!["downloadUrl"],
+              'longitude': document.data()!['location'].longitude,
+              'latitude': document.data()!['location'].latitude,
+              'distance': distance
+            };
+            return photo;
+          }).toList();
+          photoList.sort((a, b) => a['distance'].compareTo(b['distance']));
           return new Stack(children: [
             ListView(
-              children: snapshot.data!.docs.map((document) {
+              children: photoList.map((document) {
                 return new GestureDetector(
                     onTap: () {
                       Navigator.pushNamed(context, Routes.photo_detail,
                           arguments: {
-                            'path': document.data()!["downloadUrl"],
-                            'longitude': document.data()!['location'].longitude,
-                            'latitude': document.data()!['location'].latitude
+                            'path': document['path'],
+                            'longitude': document['longitude'],
+                            'latitude': document['latitude']
                           });
                     },
                     child: PhotoContainer(
-                      path: document.data()!["downloadUrl"],
+                      path: document['path'],
                       marginTop: 9,
                       marginRight: 9,
                       height: 262,
@@ -78,5 +107,12 @@ class Feed extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(10.0))),
       ),
     ));
+  }
+
+  Future<void> _determinePosition() async {
+    var position = await Geolocator.getCurrentPosition();
+    setState(() {
+      userLocation = LatLng(position.accuracy, position.latitude);
+    });
   }
 }
